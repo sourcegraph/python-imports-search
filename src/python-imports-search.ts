af2403e804656339f1eab4506e4eeaeb6e053693
Farhan Attamimi <farhan@sourcegraph.com>
2019-12-18 10:49:25 -0800

    add prettier

--- src/python-imports-search.ts
+++ src/python-imports-search.ts
@@ -1,54 +1,56 @@
-import { EMPTY, from, of } from 'rxjs';
-import { concatMap, toArray } from 'rxjs/operators';
+import { EMPTY, from, of } from 'rxjs'
+import { concatMap, toArray } from 'rxjs/operators'
 import * as sourcegraph from 'sourcegraph'
 
 export function activate(): void {
-   sourcegraph.search.registerQueryTransformer({
-       transformQuery: (query: string) => {
-           const pyImportsRegex = /\bpy.imports:([^\s]*)/i
-           if (query.match(pyImportsRegex)) {
-               const pyImportsFilter = query.match(pyImportsRegex)
-               const pyPkg = pyImportsFilter && pyImportsFilter.length >= 1 ? pyImportsFilter[1] : ''
-               const singleImport = '^import\\s' + pyPkg + '$'
-               const namedImport = '^import\\s' + pyPkg + '\\sas\\s[^\\s]*'
-               const scopedImport = '^from\\s(.*)\\simport\\s(.*)' + pyPkg
-               return query.replace(pyImportsRegex  , `(${singleImport}|${namedImport}|${scopedImport}) lang:python `)
-           }
-           return query
-        }
-   })
+    sourcegraph.search.registerQueryTransformer({
+        transformQuery: (query: string) => {
+            console.log('transforming query')
+            const pyImportsRegex = /\bpy.imports:([^\s]*)/i
+            console.log('query', query)
+            if (query.match(pyImportsRegex)) {
+                const pyImportsFilter = query.match(pyImportsRegex)
+                const pyPkg = pyImportsFilter && pyImportsFilter.length >= 1 ? pyImportsFilter[1] : ''
+                const singleImport = '^import\\s' + pyPkg + '$'
+                const namedImport = '^import\\s' + pyPkg + '\\sas\\s[^\\s]*'
+                const scopedImport = '^from\\s(.*)\\simport\\s(.*)' + pyPkg
+                return query.replace(pyImportsRegex, `(${singleImport}|${namedImport}|${scopedImport}) lang:python `)
+            }
+            console.log(query)
+            return query
+        },
+    })
 
-   sourcegraph.workspace.onDidOpenTextDocument.subscribe(doc => {
-       if (doc.languageId !== 'python') {
+    sourcegraph.workspace.onDidOpenTextDocument.subscribe(doc => {
+        console.log(doc.languageId)
+        if (doc.languageId !== 'python') {
             return
         }
-        from(doc.text.split('\n')).pipe(
-            concatMap(
-                (line, lineNumber) => {
+        console.log('on did open doc')
+        from(doc.text.split('\n'))
+            .pipe(
+                concatMap((line, lineNumber) => {
                     const pyModRegex = /^import\s([^\s]*)$|^import\s([^\s]*)\sas\s[^\\s]*|^from\s[^\s]*\simport\s([^\s^,]*)/
-                    console.log(line.match(pyModRegex))
-                    const match = pyModRegex.exec(line);
+                    const match = pyModRegex.exec(line)
                     if (match && match.length > 1) {
                         // The match index depends on which regex pattern actually produced a match
                         const pkgName = match[1] ? match[1] : match[2] ? match[2] : match[3]
-                        return of({lineNumber, pkgName});
+                        return of({ lineNumber, pkgName })
                     }
-                    return EMPTY;
+                    return EMPTY
+                }),
+                toArray()
+            )
+            .subscribe(matches => {
+                console.log('matches', matches)
+                if (!matches) {
+                    return
                 }
-            ),
-            toArray()
-        ).subscribe(matches => {
-            if (!matches) {
-                return
-            }
-            if (
-                sourcegraph.app.activeWindow &&
-                sourcegraph.app.activeWindow.visibleViewComponents.length >
-                    0
-            ) {
-                sourcegraph.app.activeWindow.visibleViewComponents[0].setDecorations(
-                    null,
-                    matches.map(match => ({
+                if (sourcegraph.app.activeWindow && sourcegraph.app.activeWindow.visibleViewComponents.length > 0) {
+                    console.log('matching decorations')
+                    sourcegraph.app.activeWindow.visibleViewComponents[0].setDecorations(
+                        null,
+                        matches.map(match => ({
                             range: new sourcegraph.Range(
                                 new sourcegraph.Position(match.lineNumber, 0),
                                 new sourcegraph.Position(match.lineNumber, 0)
@@ -57,11 +59,11 @@ export function activate(): void {
                                 contentText: 'See all usages',
                                 linkURL: '/search?q=py.imports:' + match.pkgName,
                                 backgroundColor: 'pink',
-                                color: 'black'
-                            }
-                    })
-                ))
-            }
-            });
-    });
+                                color: 'black',
+                            },
+                        }))
+                    )
+                }
+            })
+    })
 }
