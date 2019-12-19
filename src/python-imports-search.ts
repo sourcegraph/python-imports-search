c5fb72490b58ce9e57caccc3ef885f3b91927167
Farhan Attamimi <farhan@sourcegraph.com>
2019-12-18 20:08:23 -0800

    Don't nest subscribes

--- src/python-imports-search.ts
+++ src/python-imports-search.ts
@@ -1,5 +1,5 @@
-import { EMPTY, from, of } from 'rxjs'
-import { concatMap, filter, map, switchMap, toArray } from 'rxjs/operators'
+import { from, Subscription } from 'rxjs'
+import { filter, map, switchMap } from 'rxjs/operators'
 import * as sourcegraph from 'sourcegraph'
 import { resolveSettings, Settings } from './settings'
 
@@ -7,24 +7,28 @@ const decorationType = sourcegraph.app.createDecorationType && sourcegraph.app.c
 const settings = resolveSettings(sourcegraph.configuration.get<Settings>().value)
 
 export function activate(): void {
-    sourcegraph.search.registerQueryTransformer({
-        transformQuery: (query: string) => {
-            const pyImportsRegex = /\bpy.imports:([^\s]*)/i
-            if (query.match(pyImportsRegex)) {
-                const pyImportsFilter = query.match(pyImportsRegex)
-                const pyPkg = pyImportsFilter && pyImportsFilter.length >= 1 ? pyImportsFilter[1] : ''
-                const singleImport = '^import\\s' + pyPkg + '$'
-                const namedImport = '^import\\s' + pyPkg + '\\sas\\s[^\\s]*'
-                const scopedImport = '^from\\s(.*)\\simport\\s(.*)' + pyPkg
+    const subscriptions = new Subscription()
 
-                return query.replace(
-                    pyImportsRegex,
-                    `(${singleImport}|${namedImport}|${scopedImport}) lang:python patternType:regexp`
-                )
-            }
-            return query
-        },
-    })
+    subscriptions.add(
+        sourcegraph.search.registerQueryTransformer({
+            transformQuery: (query: string) => {
+                const pyImportsRegex = /\bpy.imports:([^\s]*)/i
+                if (query.match(pyImportsRegex)) {
+                    const pyImportsFilter = query.match(pyImportsRegex)
+                    const pyPkg = pyImportsFilter && pyImportsFilter.length >= 1 ? pyImportsFilter[1] : ''
+                    const singleImport = '^import\\s' + pyPkg + '$'
+                    const namedImport = '^import\\s' + pyPkg + '\\sas\\s[^\\s]*'
+                    const scopedImport = '^from\\s(.*)\\simport\\s(.*)' + pyPkg
+
+                    return query.replace(
+                        pyImportsRegex,
+                        `(${singleImport}|${namedImport}|${scopedImport}) lang:python patternType:regexp `
+                    )
+                }
+                return query
+            },
+        })
+    )
 
     const editorsChanges = sourcegraph.app.activeWindowChanges
         ? from(sourcegraph.app.activeWindowChanges).pipe(
@@ -35,57 +39,53 @@ export function activate(): void {
                   from(activeWindow.activeViewComponentChanges).pipe(map(() => activeWindow.visibleViewComponents))
               )
           )
-        : from(sourcegraph.workspace.onDidOpenTextDocument).pipe(
+        : from(sourcegraph.workspace.openedTextDocuments).pipe(
               map(() => (sourcegraph.app.activeWindow && sourcegraph.app.activeWindow.visibleViewComponents) || [])
           )
 
-    editorsChanges.subscribe(codeEditor => {
-        if (!settings['pyImports.showAllUsagesLinks']) {
-            return
-        }
+    subscriptions.add(
+        editorsChanges.subscribe(codeEditors => {
+            const codeEditor = codeEditors[0]
+            if (!settings['pyImports.showAllUsagesLinks']) {
+                return
+            }
 
-        const document = codeEditor[0].document
-        if (document.languageId !== 'python') {
-            return
-        }
+            const document = codeEditor.document
+            if (document.languageId !== 'python') {
+                return
+            }
 
-        if (document.text) {
-            from(document.text.split('\n'))
-                .pipe(
-                    concatMap((line, lineNumber) => {
-                        const pyModRegex = /^import\s([^\s]*)$|^import\s([^\s]*)\sas\s[^\\s]*|^from\s[^\s]*\simport\s([^\s^,]*)/
-                        const match = pyModRegex.exec(line)
-                        if (match && match.length > 1) {
-                            // The match index depends on which regex pattern actually produced a match
-                            const pkgName = match[1] ? match[1] : match[2] ? match[2] : match[3]
-                            return of({ lineNumber, pkgName })
-                        }
-                        return EMPTY
-                    }),
-                    toArray()
-                )
-                .subscribe(matches => {
-                    if (!matches) {
-                        return
-                    }
-                    if (codeEditor.length > 0) {
-                        codeEditor[0].setDecorations(
-                            decorationType,
-                            matches.map(match => ({
-                                range: new sourcegraph.Range(
-                                    new sourcegraph.Position(match.lineNumber, 0),
-                                    new sourcegraph.Position(match.lineNumber, 0)
-                                ),
-                                after: {
-                                    contentText: 'See all usages',
-                                    linkURL: '/search?q=py.imports:' + match.pkgName + '&patternType=regexp',
-                                    backgroundColor: 'pink',
-                                    color: 'black',
-                                },
-                            }))
-                        )
+            const matches: { lineNumber: number; pkgName: string }[] = []
+            if (codeEditor && document.text) {
+                const lines = document.text.split('\n')
+                lines.map((line, lineNumber) => {
+                    const pyModRegex = /^import\s([^\s]*)$|^import\s([^\s]*)\sas\s[^\\s]*|^from\s[^\s]*\simport\s([^\s^,]*)/
+                    const match = pyModRegex.exec(line)
+                    if (match && match.length > 1) {
+                        // The match index depends on which regex pattern actually produced a match
+                        const pkgName = match[1] ? match[1] : match[2] ? match[2] : match[3]
+                        matches.push({ lineNumber, pkgName })
                     }
                 })
-        }
-    })
+
+                if (matches.length > 0) {
+                    codeEditor.setDecorations(
+                        decorationType,
+                        matches.map(match => ({
+                            range: new sourcegraph.Range(
+                                new sourcegraph.Position(match.lineNumber, 0),
+                                new sourcegraph.Position(match.lineNumber, 0)
+                            ),
+                            after: {
+                                contentText: 'See all usages',
+                                linkURL: '/search?q=py.imports:' + match.pkgName + '&patternType=regexp',
+                                backgroundColor: 'pink',
+                                color: 'black',
+                            },
+                        }))
+                    )
+                }
+            }
+        })
+    )
 }
