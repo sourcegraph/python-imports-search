8b97f26077e831666bf87d0bf6766eb936199689
Farhan Attamimi <farhan@sourcegraph.com>
2019-12-19 10:16:45 -0800

    Update all editors

--- src/python-imports-search.ts
+++ src/python-imports-search.ts
@@ -47,47 +47,48 @@ export function activate(
 
     context.subscriptions.add(
         editorsChanges.subscribe(codeEditors => {
-            const codeEditor = codeEditors[0]
-            if (!settings['pyImports.showAllUsagesLinks']) {
-                return
-            }
-
-            const document = codeEditor.document
-            if (document.languageId !== 'python') {
-                return
-            }
-
-            const matches: { lineNumber: number; pkgName: string }[] = []
-            if (codeEditor && document.text) {
-                const lines = document.text.split('\n')
-                lines.map((line, lineNumber) => {
-                    const pyModRegex = /^import\s([^\s]*)$|^import\s([^\s]*)\sas\s[^\\s]*|^from\s[^\s]*\simport\s([^\s^,]*)/
-                    const match = pyModRegex.exec(line)
-                    if (match && match.length > 1) {
-                        // The match index depends on which regex pattern actually produced a match
-                        const pkgName = match[1] ? match[1] : match[2] ? match[2] : match[3]
-                        matches.push({ lineNumber, pkgName })
-                    }
-                })
-
-                if (matches.length > 0) {
-                    codeEditor.setDecorations(
-                        decorationType,
-                        matches.map(match => ({
-                            range: new sourcegraph.Range(
-                                new sourcegraph.Position(match.lineNumber, 0),
-                                new sourcegraph.Position(match.lineNumber, 0)
-                            ),
-                            after: {
-                                contentText: 'See all usages',
-                                linkURL: '/search?q=py.imports:' + match.pkgName + '&patternType=regexp',
-                                backgroundColor: 'pink',
-                                color: 'black',
-                            },
-                        }))
-                    )
+            codeEditors.map(codeEditor => {
+                if (!settings['pyImports.showAllUsagesLinks']) {
+                    return
                 }
-            }
+
+                const document = codeEditor.document
+                if (document.languageId !== 'python') {
+                    return
+                }
+
+                const matches: { lineNumber: number; pkgName: string }[] = []
+                if (codeEditor && document.text) {
+                    const lines = document.text.split('\n')
+                    lines.map((line, lineNumber) => {
+                        const pyModRegex = /^import\s([^\s]*)$|^import\s([^\s]*)\sas\s[^\\s]*|^from\s[^\s]*\simport\s([^\s^,]*)/
+                        const match = pyModRegex.exec(line)
+                        if (match && match.length > 1) {
+                            // The match index depends on which regex pattern actually produced a match
+                            const pkgName = match[1] ? match[1] : match[2] ? match[2] : match[3]
+                            matches.push({ lineNumber, pkgName })
+                        }
+                    })
+
+                    if (matches.length > 0) {
+                        codeEditor.setDecorations(
+                            decorationType,
+                            matches.map(match => ({
+                                range: new sourcegraph.Range(
+                                    new sourcegraph.Position(match.lineNumber, 0),
+                                    new sourcegraph.Position(match.lineNumber, 0)
+                                ),
+                                after: {
+                                    contentText: 'See all usages',
+                                    linkURL: '/search?q=py.imports:' + match.pkgName + '&patternType=regexp',
+                                    backgroundColor: 'pink',
+                                    color: 'black',
+                                },
+                            }))
+                        )
+                    }
+                }
+            })
         })
     )
 }
