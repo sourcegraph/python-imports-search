96cae8cd8546590220ba78dd2331435c8b242dbe
TJ Kandala <kandalatj@gmail.com>
2021-09-14 20:54:18 -0400

    refactor: single condition early return

--- src/python-imports-search.ts
+++ src/python-imports-search.ts
@@ -48,50 +48,46 @@ export function activate(
     context.subscriptions.add(
         editorsChanges.subscribe(codeEditors => {
             codeEditors.map(codeEditor => {
-                if (!settings['pyImports.showAllUsagesLinks']) {
+                if (
+                    !settings['pyImports.showAllUsagesLinks'] ||
+                    codeEditor.type !== 'CodeEditor' ||
+                    codeEditor.document?.languageId !== 'python' ||
+                    !codeEditor.document.text
+                ) {
                     return
                 }
 
-                if (codeEditor.type !== 'CodeEditor') {
-                    return
-                }
-
-                const document = codeEditor.document
-                if (document.languageId !== 'python') {
-                    return
-                }
+                const text = codeEditor.document.text
 
                 const matches: { lineNumber: number; pkgName: string }[] = []
-                if (document.text) {
-                    const lines = document.text.split('\n')
-                    lines.map((line, lineNumber) => {
-                        const pyModRegex =
-                            /^import\s([^\s]*)$|^import\s([^\s]*)\sas\s[^\\s]*|^from\s[^\s]*\simport\s([^\s^,]*)/
-                        const match = pyModRegex.exec(line)
-                        if (match && match.length > 1) {
-                            // The match index depends on which regex pattern actually produced a match
-                            const pkgName = match[1] ? match[1] : match[2] ? match[2] : match[3]
-                            matches.push({ lineNumber, pkgName })
-                        }
-                    })
-
-                    if (matches.length > 0) {
-                        codeEditor.setDecorations(
-                            decorationType,
-                            matches.map(match => ({
-                                range: new sourcegraph.Range(
-                                    new sourcegraph.Position(match.lineNumber, 0),
-                                    new sourcegraph.Position(match.lineNumber, 0)
-                                ),
-                                after: {
-                                    contentText: 'See all usages',
-                                    linkURL: '/search?q=py.imports:' + match.pkgName + '&patternType=regexp',
-                                    backgroundColor: 'pink',
-                                    color: 'black',
-                                },
-                            }))
-                        )
+                const lines = text.split('\n')
+                lines.map((line, lineNumber) => {
+                    const pyModRegex =
+                        /^import\s([^\s]*)$|^import\s([^\s]*)\sas\s[^\\s]*|^from\s[^\s]*\simport\s([^\s^,]*)/
+                    const match = pyModRegex.exec(line)
+                    if (match && match.length > 1) {
+                        // The match index depends on which regex pattern actually produced a match
+                        const pkgName = match[1] ? match[1] : match[2] ? match[2] : match[3]
+                        matches.push({ lineNumber, pkgName })
                     }
+                })
+
+                if (matches.length > 0) {
+                    codeEditor.setDecorations(
+                        decorationType,
+                        matches.map(match => ({
+                            range: new sourcegraph.Range(
+                                new sourcegraph.Position(match.lineNumber, 0),
+                                new sourcegraph.Position(match.lineNumber, 0)
+                            ),
+                            after: {
+                                contentText: 'See all usages',
+                                linkURL: '/search?q=py.imports:' + match.pkgName + '&patternType=regexp',
+                                backgroundColor: 'pink',
+                                color: 'black',
+                            },
+                        }))
+                    )
                 }
             })
         })
